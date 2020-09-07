3dcbc612ef496fa238e9de609f8c68dea295124b
Danny Mack <drmterps@yahoo.com>
2020-09-07 01:36:27 -0400

    Tweak removeFirst(where:) algorithm

--- Sources/SwiftTreys/Extensions.swift
+++ Sources/SwiftTreys/Extensions.swift
@@ -18,14 +18,7 @@ extension Array {
     @discardableResult mutating func removeFirst(where f: (Element) throws -> Bool) rethrows
         -> Element?
     {
-        var copy = self
-        for (index, el) in self.enumerated() {
-            if try f(el) {
-                let removed = copy.remove(at: index)
-                self = copy
-                return removed
-            }
-        }
+        for (index, el) in self.enumerated() { if try f(el) { return remove(at: index) } }
         return nil
     }
 }
@@ -33,8 +26,6 @@ extension Array {
 extension Dictionary where Value: Hashable {
     func swappingKeysAndValues() -> [Value: Key]? {
         guard Set(values).count == Set(keys).count else { return nil }
-        return reduce(into: [:]) { (res: inout [Value: Key], next) in
-            res[next.value] = next.key
-        }
+        return reduce(into: [:]) { (res: inout [Value: Key], next) in res[next.value] = next.key }
     }
 }
